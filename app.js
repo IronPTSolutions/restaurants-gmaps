226b230b5a585eec22d1ea66cece1706357bfd01
Julio García Pérez <juliojose.garcia@11paths.com>
2018-01-26 20:47:58 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const expressLayout = require('express-ejs-layouts');
+
+// Require routes
+
+// Import DB config
+require('./config/db.config');
+
+const app = express();
+
+// view engine setup
+app.use(expressLayout);
+app.set('layout', 'layout');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Middlewares
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+
+
+module.exports = app;
